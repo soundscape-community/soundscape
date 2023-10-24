2da2f4d5ca5d0806dc160355fd02a91cbcff6f1a
Robbie Murray <rob@mur.org.uk>
2023-10-24 17:14:54 +0100

    change service url to https://tiles.soundscape.services

--- apps/ios/GuideDogs/Code/Data/Services/Helpers/ServiceModel.swift
+++ apps/ios/GuideDogs/Code/Data/Services/Helpers/ServiceModel.swift
@@ -34,7 +34,7 @@ class ServiceModel {
     static let errorRealm = "GDAHTTPErrorRealm"
     
     /// Domain name to resolve for production services
-    private static let productionServicesHostName = "https://newprod0.openscape.io"
+    private static let productionServicesHostName = "https://tiles.soundscape.services"
     /// Domain part of the URL for learning resources
     private static let productionAssestsHostName = "https://soundscape.services"
     // Do not change `productionVoicesHostName`!
