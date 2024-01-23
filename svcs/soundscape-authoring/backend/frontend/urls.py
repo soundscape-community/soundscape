6943d8806715ade2bf7ebfe29565a797c486e0ed
Robbie Murray <rob@mur.org.uk>
2024-01-23 09:38:24 +0000

    Remove svcs/soundscape-authoring since it has it's own repository at soundscape-community/authoring-tool

--- svcs/soundscape-authoring/backend/frontend/urls.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-
-from django.urls import path
-from .views import AppView
-
-urlpatterns = [
-    path('', AppView.as_view()),
-]
