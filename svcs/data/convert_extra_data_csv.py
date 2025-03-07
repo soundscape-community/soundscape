118509d6fc63d62b663e558f359416eec80d9682
Daniel W. Steinbrook <dsteinbrook@gmail.com>
2025-03-07 08:36:13 -0500

    Add additional fields to converted NaviLens CSV

--- svcs/data/convert_extra_data_csv.py
+++ svcs/data/convert_extra_data_csv.py
@@ -2,18 +2,13 @@
 # Convert CSV into expected format for non-OSM data ingestion.
 #
 # Example usage:
-#    python convert_extra_data_csv.py mobility navilens input.csv output.csv
-#
-# This will set feature_type to "mobility" and feature_value to "navilens"
-# for all rows.
+#    python convert_extra_data_csv.py input.csv output.csv
 
 import argparse
 import csv
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("feature_type")
-    parser.add_argument("feature_value")
     parser.add_argument("input_csv")
     parser.add_argument("output_csv")
     args = parser.parse_args()
@@ -23,14 +18,24 @@ if __name__ == "__main__":
 
         with open(args.output_csv, "w", newline="") as f:
             outcsv = csv.DictWriter(f, fieldnames=[
-                "feature_type", "feature_value", "latitude", "longitude", 
+                "feature_type", "feature_value", "latitude", "longitude",
+                "name", "navilens", "bus", "highway", "public_transport",
             ] + incsv.fieldnames)
             outcsv.writeheader()
             for row in incsv:
                 outcsv.writerow({
-                    "latitude": row["stop_lat"],
-                    "longitude": row["stop_lon"],
-                    "feature_type": args.feature_type,
-                    "feature_value": args.feature_value,
+                    "latitude": row.pop("stop_lat"),
+                    "longitude": row.pop("stop_lon"),
+                    #TODO Generalize beyond bus stops
+                    "name": "NaviLens available: " + (
+                        row.pop("stop_desc") or row.pop("stop_name")
+                    ),
+                    "feature_type": "highway",
+                    "feature_value": "bus_stop",
+                    "navilens": "true",
+                    # properites observed on bus stops in OSM
+                    "bus": "yes",
+                    "highway": "bus_stop",
+                    "public_transport": "platform",
                     **row,
                 })
