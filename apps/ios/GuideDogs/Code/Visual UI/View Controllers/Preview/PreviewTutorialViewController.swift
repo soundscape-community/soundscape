34c1f17ed90e33edd8672b2ffb983c5399295a63
Robbie Murray <rob@mur.org.uk>
2026-07-31 18:35:22 +0100

    Respect safe area in preview tutorial

--- apps/ios/GuideDogs/Code/Visual UI/View Controllers/Preview/PreviewTutorialViewController.swift
+++ apps/ios/GuideDogs/Code/Visual UI/View Controllers/Preview/PreviewTutorialViewController.swift
@@ -19,45 +19,44 @@ struct PreviewTutorialView: View {
     let onDone: () -> Void
 
     var body: some View {
-        ZStack {
+        VStack(spacing: 0) {
+            ScrollView {
+                VStack(spacing: 48) {
+                    section(title: GDLocalizedString("preview.tutorial.title.1"),
+                            content: GDLocalizedString("preview.tutorial.content.1"),
+                            imageName: "Welcome1.gif",
+                            titleFont: .largeTitle)
+                    section(title: GDLocalizedString("preview.tutorial.title.2"),
+                            content: GDLocalizedString("preview.tutorial.content.2"),
+                            imageName: "Welcome2.gif",
+                            titleFont: .title)
+
+                    VStack(spacing: 24) {
+                        heading(GDLocalizedString("preview.tutorial.title.3"), font: .title)
+                        content(GDLocalizedString("preview.tutorial.content.3"))
+                        content(GDLocalizedString("preview.tutorial.content.4"))
+                            .padding(.top, 24)
+                    }
+                }
+                .padding(.horizontal, 28)
+                .padding(.vertical, 20)
+            }
+
+            Button(GDLocalizedString("general.alert.done"), action: onDone)
+                .font(.body)
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity, minHeight: 64)
+                .background(Color(Colors.Background.quaternary ?? .black).opacity(0.9))
+                .accessibilityHint(GDLocalizedString("preview.tutorial.done.hint"))
+                .padding(.horizontal, 20)
+                .background(Color.black.opacity(0.3).ignoresSafeArea())
+        }
+        .background {
             Image("Background")
                 .resizable()
                 .scaledToFill()
                 .ignoresSafeArea()
                 .accessibilityHidden(true)
-
-            VStack(spacing: 0) {
-                ScrollView {
-                    VStack(spacing: 48) {
-                        section(title: GDLocalizedString("preview.tutorial.title.1"),
-                                content: GDLocalizedString("preview.tutorial.content.1"),
-                                imageName: "Welcome1.gif",
-                                titleFont: .largeTitle)
-                        section(title: GDLocalizedString("preview.tutorial.title.2"),
-                                content: GDLocalizedString("preview.tutorial.content.2"),
-                                imageName: "Welcome2.gif",
-                                titleFont: .title)
-
-                        VStack(spacing: 24) {
-                            heading(GDLocalizedString("preview.tutorial.title.3"), font: .title)
-                            content(GDLocalizedString("preview.tutorial.content.3"))
-                            content(GDLocalizedString("preview.tutorial.content.4"))
-                                .padding(.top, 24)
-                        }
-                    }
-                    .padding(.horizontal, 28)
-                    .padding(.vertical, 20)
-                }
-
-                Button(GDLocalizedString("general.alert.done"), action: onDone)
-                    .font(.body)
-                    .foregroundColor(.white)
-                    .frame(maxWidth: .infinity, minHeight: 64)
-                    .background(Color(Colors.Background.quaternary ?? .black).opacity(0.9))
-                    .accessibilityHint(GDLocalizedString("preview.tutorial.done.hint"))
-                    .padding(.horizontal, 20)
-                    .background(Color.black.opacity(0.3).ignoresSafeArea())
-            }
         }
     }
 
@@ -120,7 +119,7 @@ class PreviewTutorialViewController: UIViewController {
         NSLayoutConstraint.activate([
             hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostingController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
         hostingController.didMove(toParent: self)
